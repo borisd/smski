e74a51fca3d65fe112a3b6fbec7ab08f23b8cb70
unknown <Murkin@.(none)>
2009-12-06 11:26:15 +0200

    Added the logs implementation

---
+++ log.py
@@ -0,0 +1,31 @@
+from myproject.smski.models import DataLog
+
+from django.contrib.auth.models import User
+
+import logging
+import datetime
+
+log = 0
+
+class db_handler(logging.Handler):
+    def __init__(self, user=None):
+        logging.Handler.__init__(self)
+        if user is None:
+            user = User.objects.filter(username='admin')[0]
+        self.user = user
+
+    def emit(self, record):
+        msg = self.format(record)
+        date = datetime.datetime.now()
+        entry = DataLog(msg=msg, date=date, user=self.user)
+        entry.save()
+
+def log_init():
+    global log
+
+    log = logging.getLogger('main')
+    log.addHandler(db_handler())
+    log.setLevel(logging.DEBUG)
+
+log_init()
+
